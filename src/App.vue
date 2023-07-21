8a35c24c131b17dc4fead207955ab53b02c1a531
Ivan_Ma <ivan@rexlite.com.tw>
2023-07-21 13:32:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HomePage msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #2c3e50;
+  color: aliceblue;
+  /* color: #2c3e50; */
+  margin-top: 60px;
+}
+*{outline: 2px red solid;}
+html {
+  background-color: #2c3e50;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <h1>多人帳務系統</h1>
+    <div>
+      <div class="input-group">
+        <input type="text" class="form-control " v-model="newUsername" placeholder="輸入使用者名稱">
+        <button class="btn btn-success" @click="addUser">新增使用者</button>
+      </div>
+    </div>
+    <div>
+      <button class="btn btn-primary " style="margin: 10px;" v-for="user in users" :key="user.id">使用者 ID: {{ user.id }}，名稱: {{ user.name
+      }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      newUsername: "", // 新增使用者的名稱
+      users: [], // 使用者清單陣列
+    }
+  },
+  methods: {
+    addUser() {
+      const username = this.newUsername.trim();
+
+      if (username !== "") {
+        // 建立使用者物件並加入使用者清單
+        const user = { id: this.users.length + 1, name: username };
+        this.users.push(user);
+
+        // 清空輸入欄位
+        this.newUsername = "";
+      }
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
